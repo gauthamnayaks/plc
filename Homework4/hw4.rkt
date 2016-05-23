7295c6659109c1f8e7ad6817e2db89aeed30fbc1
Gautham <gautham.nayak@outlook.com>
2016-05-23 09:19:22 +0200

    till q 9

--- Homework4/hw4.rkt
+++ Homework4/hw4.rkt
@@ -21,4 +21,36 @@
          (car (list-tail xs (remainder n (length xs)))))))
 
 (define (stream-for-n-steps s n)
+  (if (<= n 0)
+      null
+      (let ([nexts (s)])
+            (cons (car nexts) (stream-for-n-steps (cdr nexts) (- n 1))))))
+
+(define funny-number-stream
+        (letrec ([f (lambda (x) (cons (if (= (remainder x 5) 0)
+                                         (- 0 x)
+                                         x)
+                                      (lambda () (f (+ x 1)))))])
+(lambda () (f 1))))
   
+(define dan-than-dog
+  (lambda () (cons "dan.jpg"
+                   (lambda () (cons "dog.jpg" dan-than-dog)))))
+
+(define (stream-add-zero s)
+  (let ([nexts (s)])
+    (lambda () (cons (cons 0 (car nexts)) (stream-add-zero (cdr nexts))))))
+
+(define (cycle-lists xs ys)
+  (letrec ([f (lambda(n)
+                (cons (cons (list-nth-mod xs n) (list-nth-mod ys n))
+                      (lambda() (f (+ n 1)))))])
+    (lambda () (f 0))))
+
+(define (vector-assoc v vec)
+  (letrec ([f (lambda (n)
+                (cond [equal? n (vector-length vec) #f]
+                      [(pair? (vector-ref vec n)) (if (equal? (car (vector-ref vec n)) v)
+                                                      (vector-ref vec n) (f (+ n 1)))]
+                      [#t (f (+ n 1))]))])
+    (f 0)))
