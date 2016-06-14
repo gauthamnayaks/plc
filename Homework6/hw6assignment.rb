57a27afdfd04d9ee9941371c71c1c4a6f73c0c13
gauthamnayaks <gautham@control.lth.se>
2016-06-14 18:45:11 +0200

    some progress

--- Homework6/hw6assignment.rb
+++ Homework6/hw6assignment.rb
@@ -5,25 +5,63 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-  All_Pieces = [#[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-               #rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-               #[[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-               #[[0, 0], [0, -1], [0, 1], [0, 2]]],
-               #rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-               #rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-               #rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-               #rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]),
-                [[[0,0],[0,1]],[1,0],[0,0]]] # Z
+  All_Pieces = Piece::All_Pieces.concat( # Enhancement 2
+                [[[[0, 0], [-1, 0], [1, 0], [2, 0], [-2,0]], # long 5 (only needs two)
+                [[0, 0], [0, -1], [0, 1], [0, 2], [0, -2]]],
+                rotations([[0, 0], [0, 1], [1, 0]]), # 3 block
+                rotations([[0, 0], [0, 1], [-1, 0], [1, 0], [-1, 1]])])
   # your enhancements here
-
+  def self.cheat_piece (board)
+  #  if @cheated
+  #  MyPiece.new([[[0,0]]], board)
+  #  else
+    MyPiece.new([[[0,0]]], board)
+  # end
+  end
 end
 
 class MyBoard < Board
   # your enhancements here
+  def initialize (game)
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+  end
 
+  def cheat_piece
+    if !game_over? and @game.is_running?
+      @cheated = true
+    end
+  end
+
+  def next_piece
+    if @cheated
+      @current_block = MyPiece.cheat_piece(self)
+      @current_pos = nil
+      @cheated = false
+      @score -= 100
+    else
+      @current_block = MyPiece.next_piece(self)
+      @current_pos = nil
+    end
+  end
 end
 
 class MyTetris < Tetris
   # your enhancements here
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
 
+  def key_bindings
+    super()
+    @root.bind('u', lambda {@board.rotate_counter_clockwise;@board.rotate_counter_clockwise}) #Enhancement 1
+    @root.bind('c', lambda {@board.cheat_piece})
+  end
 end
